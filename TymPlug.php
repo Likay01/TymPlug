0c944526162318d5408103066b04cc62d510bb2d
Likay01 <mgoqilindokuhle@gmail.com>
2023-05-15 11:09:06 +0200

    done

--- TymPlug.php
+++ TymPlug.php
@@ -12,6 +12,23 @@
  function sa_time_shortcode() {
     date_default_timezone_set('Africa/Johannesburg');
     $current_time = date('h:i:s A');
-    return '<p>Current time in South Africa is: ' . $current_time . '</p>';
+    return '<p> Site Achieved at : ' . $current_time . ' TymPlug</p>';
 }
-add_shortcode('sa_time', 'sa_time_shortcode');
+add_shortcode('tym', 'sa_time_shortcode');
+ 
+add_action( 'admin_menu', 'wporg_options_page' );
+function wporg_options_page() {
+    add_menu_page(
+        'TymPlug',
+        'TymPlug',
+        'manage_options',
+        'wporg',
+        'wporg_options_page_html',
+        'dashicons-share-alt',
+        20
+    );
+}
+function wporg_options_page_html () {
+    echo '<h1>TymPlug</h1>';
+    echo '<p>A simple plugin to display the current time.</p>';
+}
